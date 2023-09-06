70301a293eeaed6937f3cca3ae5c4a29e38d9045
JJeonBH <wwjjss5386@gmail.com>
2023-09-06 17:44:52 +0900

    7장 9절 객체 타입 확인

--- src/ch07/sec08/exam02/DriverExample.java
+++ src/ch07/sec08/exam02/DriverExample.java
@@ -5,11 +5,11 @@ public class DriverExample {
 		//	Driver 객체 생성
 		Driver driver = new Driver();
 		
-		//	매개값으로 Bus 객체를 제공하고 run() 메소드 호출
+		//	매개값으로 Bus 객체를 제공하고 drive() 메소드 호출
 		Bus bus = new Bus();
 		driver.drive(bus);
 		
-		//	매개값으로 Taxi 객체를 제공하고 run() 메소드 호출
+		//	매개값으로 Taxi 객체를 제공하고 drive() 메소드 호출
 		Taxi taxi = new Taxi();
 		driver.drive(taxi);
 	}
