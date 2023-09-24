9a25fb1229254e43728672dc84921c0e7da2a405
JJeonBH <wwjjss5386@gmail.com>
2023-09-24 20:29:57 +0900

    13장 2절 제네릭 타입

--- src/ch12/sec12/PrintAnnotation.java
+++ src/ch12/sec12/PrintAnnotation.java
@@ -5,9 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
+@Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PrintAnnotation {
 	String value() default "-";
+
 	int number() default 15;
 }
