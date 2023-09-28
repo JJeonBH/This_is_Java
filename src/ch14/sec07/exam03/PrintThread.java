d484fce0924fa2648f2dca91b66459c293492287
JJeonBH <wwjjss5386@gmail.com>
2023-09-28 15:49:15 +0900

    14장 7절 스레드 안전 종료

---
+++ src/ch14/sec07/exam03/PrintThread.java
@@ -0,0 +1,15 @@
+package ch14.sec07.exam03;
+
+public class PrintThread extends Thread {
+	@Override
+	public void run() {
+		while (true) {
+			System.out.println("실행 중");
+			if (Thread.interrupted()) {
+				break;
+			}
+		}
+		System.out.println("리소스 정리");
+		System.out.println("실행 종료");
+	}
+}
