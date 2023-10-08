54a9ba200ce6713af99d3d07f963dd71d57d2228
JJeonBH <wwjjss5386@gmail.com>
2023-10-08 18:10:27 +0900

    16장 5절 메소드 참조

---
+++ src/ch16/sec05/exam02/MethodReferenceExample.java
@@ -0,0 +1,9 @@
+package ch16.sec05.exam02;
+
+public class MethodReferenceExample {
+	public static void main(String[] args) {
+		Person person = new Person();
+		// person.ordering((a, b) -> a.compareToIgnoreCase(b));
+		person.ordering(String::compareToIgnoreCase);
+	}
+}
