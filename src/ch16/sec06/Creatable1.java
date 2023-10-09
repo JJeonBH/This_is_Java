5adbd1ffb545a4c6e882a12948e8483b027438f0
JJeonBH <wwjjss5386@gmail.com>
2023-10-09 17:23:55 +0900

    16장 6절 생성자 참조

---
+++ src/ch16/sec06/Creatable1.java
@@ -0,0 +1,6 @@
+package ch16.sec06;
+
+@FunctionalInterface
+public interface Creatable1 {
+	public Member create(String id);
+}
---
+++ src/ch16/sec06/Creatable2.java
@@ -0,0 +1,6 @@
+package ch16.sec06;
+
+@FunctionalInterface
+public interface Creatable2 {
+	public Member create(String id, String name);
+}
